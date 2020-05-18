ce8ae3ad005a35083dc88d0233cbd9e744fd13e5
Pkthunderr <jared_perreault@yahoo.com>
2020-05-18 12:36:58 -0400

    [BUG] replaced JSON.stringify with JSON.parse

--- lambda/index.js
+++ lambda/index.js
@@ -2,11 +2,11 @@ const AWS = require('aws-sdk');
 
 const ses = new AWS.SESV2();
 
+const Charset = 'UTF-8';
+
 const ensureArray = (val) => val instanceof Array ? val : [val];
 
 const eventToSESParams = (obj) => {
-  const Charset = 'UTF-8';
-
   let content = {};
   if (obj.body) {
     content = {
@@ -37,19 +37,18 @@ const eventToSESParams = (obj) => {
       CcAddresses: ensureArray(obj.cc),
       BccAddresses: ensureArray(obj.bcc),
     },
-    FromEmailAddress: obj.from
+    FromEmailAddress: obj.from || obj.as || process.env.DEFAULT_FROM_ADDRESS
   };
 }
 
 exports.handler = async (event) => {
   const emails = await Promise.all(event.Records.map(r => {
-    const event = {...JSON.stringify(r.body)}
-    event.from = event.from || event.as || process.env.DEFAULT_FROM_ADDRESS;
-    return ses.sendMail(eventToSESParams(event)).promise();
+    const emailOpts = {...JSON.parse(r.body)}
+    return ses.sendEmail(eventToSESParams(emailOpts)).promise();
   }));
 
   return {
     statusCode: 200,
-    body: emails.length
+    body: `${emails.length} sent`
   }
 };
